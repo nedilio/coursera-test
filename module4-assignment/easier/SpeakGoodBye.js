df4a0160737b3d38647ffaaa547cc8ffa10a80cb
nedilio <izquierdonelson@gmail.com>
2016-05-17 21:32:32 -0430

    solution mod4 easier

--- module4-assignment/easier/SpeakGoodBye.js
+++ module4-assignment/easier/SpeakGoodBye.js
@@ -17,7 +17,7 @@
 	// byeSpeaker object instead of being a standalone function.
 	// See Lecture 52, part 2
 	byeSpeaker.speak = function (names) {
-	  console.log(speakWord + " " + byeSpeaker.name);
+	  console.log(speakWord + " " + names);
 	}
 
 	// STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
