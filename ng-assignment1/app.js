081ecf77200b47c091105c71eff1c5382e75fb99
nedilio <izquierdonelson@gmail.com>
2017-04-21 00:38:34 -0400

    Changing input color

--- ng-assignment1/app.js
+++ ng-assignment1/app.js
@@ -12,10 +12,12 @@
             if ($scope.listText == "") {
                 $scope.message = "Please enter data first";
                 $scope.myStyle={color:'red'};
+                $scope.borderStyle={border:'1px solid red'};
             } else {
                 $scope.arrayOfFood = $scope.listText.split(",");
                 $scope.foodCounter = $scope.arrayOfFood.length;
                 $scope.myStyle={color:'green'};
+                $scope.borderStyle={border:'1px solid green'};
                 if ($scope.foodCounter < 4) {
                     $scope.message = "Enjoy!";
                 } else {
