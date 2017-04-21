a3c225621317ba0b5e1eab7c725c3a78388fbaa7
nedilio <izquierdonelson@gmail.com>
2017-04-21 00:25:19 -0400

    Changing Message style color

--- ng-assignment1/app.js
+++ ng-assignment1/app.js
@@ -11,9 +11,11 @@
         $scope.ChecktooMuch = function () {
             if ($scope.listText == "") {
                 $scope.message = "Please enter data first";
+                $scope.myStyle={color:'red'};
             } else {
                 $scope.arrayOfFood = $scope.listText.split(",");
                 $scope.foodCounter = $scope.arrayOfFood.length;
+                $scope.myStyle={color:'green'};
                 if ($scope.foodCounter < 4) {
                     $scope.message = "Enjoy!";
                 } else {
